13df0148258254c9b199a1cdc62d93a208d05d0a
Adesh15 <adesikha15@gmail.com>
2018-02-04 12:25:11 +0000

    mido: Initial HalogenOS bringup
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- XOS.mk
+++ XOS.mk
@@ -16,11 +16,12 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HalogenOS stuff.
+$(call inherit-product, vendor/xos/config/common.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := XOS_mido
 BOARD_VENDOR := Xiaomi
+PRODUCT_DEVICE := mido
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
