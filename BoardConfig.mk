231c3b5f82bca08df87287af390a33ccbe167745
Adesh15 <adesikha15@gmail.com>
2018-04-25 12:19:50 +0000

    mido: Add system.prop to fix build *Add RR version and build type
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
+# Properties
+TARGET_SYSTEM_PROP += device/xiaomi/mido/system.prop
+
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
